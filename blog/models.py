af32bc5e1d55000e2462b643962116259d04d800
Max Kolle <maxkolle1@gmail.com>
2021-07-15 22:50:50 +0100

    Create models and migrate

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255, blank=True, default="")
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['created']
+
+
+class Comment(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=255, blank=True, default='admin')
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['created']
