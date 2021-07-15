d6412c75445389755367577707a09c0c1de96029
Max Kolle <maxkolle1@gmail.com>
2021-07-15 23:26:50 +0100

    Add urls for listing, showing and creating articles

--- blog/views.py
+++ blog/views.py
@@ -17,8 +17,8 @@ class ArticleList(APIView):
         serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=HTTP_201_CREATED)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ArticleDetail(APIView):
